07ff12405140374e809e418918c36adb8200976b
Serhii Olianovskyi <solianovsk@e2r7p11.unit.ua>
2019-08-10 23:38:44 +0300

    07

---
+++ inc/header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct s_player {
+    int x;
+    int y;
+    int cloneX;
+    int cloneY;
+    int dx;
+    int dy;
+} t_player;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#endif
