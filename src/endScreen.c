2894ae7173dae354c4f49991bc273ff25613f8c2
Vadym Blakyta <vblakyta@e2r6p10.unit.ua>
2019-08-14 01:44:35 +0300

    Features

--- src/endScreen.c
+++ src/endScreen.c
@@ -12,15 +12,26 @@ int endScreen(t_player *player, SDL_Renderer *renderer) {
           printf("%s",TTF_GetError());
           exit(1);
     };
+    // Again rect
+    SDL_Rect againBackground = {300, 800, 450, 150};
+    SDL_Texture *imgAgainBackground = IMG_LoadTexture(renderer, "sprites/again_button2.png");
+    // Quit rect
+    SDL_Rect quitBackground = {1100, 800, 450, 150};
+    SDL_Texture *imgQuitBackground = IMG_LoadTexture(renderer, "sprites/quit2.png");
 
+    SDL_Rect backgroundRect = {0, 0, WIDTH_PIX, HEIGHT_PIX};
+    SDL_Texture *imgBackground = IMG_LoadTexture(renderer, "sprites/end_background.png");
     while(running) {
         
         while(SDL_PollEvent(&event)) {
             if(event.key.keysym.sym == SDLK_ESCAPE) {
                     running = false;
                 }
+            // if(event.key.keysym.sym == SDLK_ESCAPE)    
         }
-        //SDL_RenderCopy(renderer, imgBorder, NULL, &textRect);
+        SDL_RenderCopy(renderer, imgBackground, NULL, &backgroundRect);
+        SDL_RenderCopy(renderer, imgAgainBackground, NULL, &againBackground);
+        SDL_RenderCopy(renderer, imgQuitBackground, NULL, &quitBackground);
         usleep(100);
         SDL_RenderPresent(renderer);
     }
--- src/showStartMenu.c
+++ src/showStartMenu.c
@@ -19,13 +19,19 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
     };*/
 
     SDL_Color color = {255, 255, 0, 255};
-    //Customize panel
-    SDL_Rect customize_backgroundRect = {150, 400, 500, 700};
-    SDL_Texture *customize_imgBackground = IMG_LoadTexture(renderer, "sprites/border2.png");
+    //Customize panels
+    SDL_Rect button1 = {715, 435, 400, 120};
+    SDL_Texture *button11 = IMG_LoadTexture(renderer, "sprites/button.png");
+    SDL_Rect button2 = {715, 585, 400, 120};
+    SDL_Texture *button22 = IMG_LoadTexture(renderer, "sprites/button.png");
+    SDL_Rect button3 = {280, 935, 400, 120};
+    SDL_Texture *button33 = IMG_LoadTexture(renderer, "sprites/button.png");
+    SDL_Rect button4 = {1160, 935, 400, 120};
+    SDL_Texture *button44 = IMG_LoadTexture(renderer, "sprites/button.png");
 
     //background 
     SDL_Rect backgroundRect = {0, 0, WIDTH_PIX, HEIGHT_PIX};
-    SDL_Texture *imgBackground = IMG_LoadTexture(renderer, "sprites/border2.png");
+    SDL_Texture *imgBackground = IMG_LoadTexture(renderer, "sprites/main_menu_background.png");
 
     while(running) {
         color.r = 255;
@@ -39,17 +45,18 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
                 }
                 if(event.key.keysym.sym == SDLK_UP) {
                     if(indexActive == 2) indexActive = 1;
-                    if(indexActive == 4) indexActive = 3;
+                    if(indexActive == 4) indexActive = 2;
+                    if(indexActive == 3) indexActive = 2;
                 }
                 if(event.key.keysym.sym == SDLK_RIGHT) {
-                    if((indexActive == 3) || (indexActive == 4)) indexActive = 1;
+                    if(indexActive == 4) indexActive = 3;
                 }
                 if(event.key.keysym.sym == SDLK_DOWN) {
+                    if(indexActive == 2) indexActive = 4;
                     if(indexActive == 1) indexActive = 2;
-                    if(indexActive == 3) indexActive = 4;
                 }
                 if(event.key.keysym.sym == SDLK_LEFT) {
-                    if((indexActive == 1) || (indexActive == 2)) indexActive = 3;
+                    if(indexActive == 3) indexActive = 4;
                 }
                 if(event.key.keysym.sym == SDLK_RETURN) {
                     return indexActive;
@@ -62,38 +69,42 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
         //SDL_RenderCopy(renderer, imgBackground, NULL, &backgroundRect);
         
         SDL_RenderCopy(renderer, imgBackground, NULL, &backgroundRect);
+        SDL_RenderCopy(renderer, button11, NULL, &button1);
+        SDL_RenderCopy(renderer, button22, NULL, &button2);
+        SDL_RenderCopy(renderer, button33, NULL, &button3);
+        SDL_RenderCopy(renderer, button44, NULL, &button4);
         //SDL_RenderCopy(renderer, customize_imgBackground, NULL, &customize_backgroundRect);
         if (indexActive == 1) {
             color.g = 0;
-            drawText(color, 1000, 500, "Easy mode", renderer, font_Customize);
+            drawText(color, 750, 450, "Easy mode", renderer, font_Customize);
             color.g = 255;
-            drawText(color, 200, 500, "Customize", renderer, font_Customize);
-            drawText(color, 200, 1000, "Scoreboard", renderer, font_Customize);
-            drawText(color, 1000, 600, "Hard mode", renderer, font_Customize);
+            drawText(color, 1200, 950, "Customize", renderer, font_Customize);
+            drawText(color, 300, 950, "Scoreboard", renderer, font_Customize);
+            drawText(color, 750, 600, "Hard mode", renderer, font_Customize);
         }
         else if(indexActive == 2) {
             color.g = 0;
-            drawText(color, 1000, 600, "Hard mode", renderer, font_Customize);
+            drawText(color, 750, 600, "Hard mode", renderer, font_Customize);
             color.g = 255;
-            drawText(color, 200, 500, "Customize", renderer, font_Customize);
-            drawText(color, 200, 1000, "Scoreboard", renderer, font_Customize);
-            drawText(color, 1000, 500, "Easy mode", renderer, font_Customize);
+            drawText(color, 1200, 950, "Customize", renderer, font_Customize);
+            drawText(color, 300, 950, "Scoreboard", renderer, font_Customize);
+            drawText(color, 750, 450, "Easy mode", renderer, font_Customize);
         }
         else if(indexActive == 3) {
             color.g = 0;
-            drawText(color, 200, 500, "Customize", renderer, font_Customize);
+            drawText(color, 1200, 950, "Customize", renderer, font_Customize);
             color.g = 255;
-            drawText(color, 1000, 600, "Hard mode", renderer, font_Customize);
-            drawText(color, 200, 1000, "Scoreboard", renderer, font_Customize);
-            drawText(color, 1000, 500, "Easy mode", renderer, font_Customize);
+            drawText(color, 750, 600, "Hard mode", renderer, font_Customize);
+            drawText(color, 300, 950, "Scoreboard", renderer, font_Customize);
+            drawText(color, 750, 450, "Easy mode", renderer, font_Customize);
         }
         else if(indexActive == 4) {
             color.g = 0;
-            drawText(color, 200, 1000, "Scoreboard", renderer, font_Customize);
+            drawText(color, 300, 950, "Scoreboard", renderer, font_Customize);
             color.g = 255;
-            drawText(color, 1000, 600, "Hard mode", renderer, font_Customize);
-            drawText(color, 200, 500, "Customize", renderer, font_Customize);
-            drawText(color, 1000, 500, "Easy mode", renderer, font_Customize);
+            drawText(color, 750, 600, "Hard mode", renderer, font_Customize);
+            drawText(color, 1200, 950, "Customize", renderer, font_Customize);
+            drawText(color, 750, 450, "Easy mode", renderer, font_Customize);
         }
         //drawText(color, 150, 600, "char *text", renderer, font_options);
         usleep(100);
