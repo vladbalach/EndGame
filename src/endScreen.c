2ef21764cbc79d9f5d3f04763a20008b16c64f1f
Vadym Blakyta <vblakyta@e2r6p10.unit.ua>
2019-08-14 02:58:57 +0300

    Music and some shit

--- src/endScreen.c
+++ src/endScreen.c
@@ -3,38 +3,70 @@
 int endScreen(t_player *player, SDL_Renderer *renderer) {
     bool running = true;
     SDL_Event event;
+    int indexActive = 5;
     if(TTF_Init()==-1) {
         printf("TTF_Init: %s\n", TTF_GetError());
         exit(1);
     }
-    TTF_Font *font = TTF_OpenFont("ttf/OpenSansBold.ttf",48);
+    TTF_Font *font = TTF_OpenFont("ttf/ofont.ru_Fixedsys.ttf",48);
     if (font == 0) {
           printf("%s",TTF_GetError());
           exit(1);
     };
+    //Border 1
+    SDL_Rect border1 = {290, 795, 380, 127};
+    SDL_Texture *border11 = IMG_LoadTexture(renderer, "sprites/ramka1.png");
+    //Border 2
+    SDL_Rect border2 = {1090, 795, 380, 127};
+    SDL_Texture *border22 = IMG_LoadTexture(renderer, "sprites/ramka4.png");
     // Again rect
-    SDL_Rect againBackground = {300, 800, 450, 150};
+    SDL_Rect againBackground = {300, 800, 360, 120};
     SDL_Texture *imgAgainBackground = IMG_LoadTexture(renderer, "sprites/again_button2.png");
     // Quit rect
-    SDL_Rect quitBackground = {1100, 800, 450, 150};
+    SDL_Rect quitBackground = {1100, 800, 360, 120};
     SDL_Texture *imgQuitBackground = IMG_LoadTexture(renderer, "sprites/quit2.png");
-
+    //Background
     SDL_Rect backgroundRect = {0, 0, WIDTH_PIX, HEIGHT_PIX};
     SDL_Texture *imgBackground = IMG_LoadTexture(renderer, "sprites/end_background.png");
+
+    
     while(running) {
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, imgBackground, NULL, &backgroundRect);
+    SDL_RenderCopy(renderer, imgAgainBackground, NULL, &againBackground);
+    SDL_RenderCopy(renderer, imgQuitBackground, NULL, &quitBackground);
         
         while(SDL_PollEvent(&event)) {
-            if(event.key.keysym.sym == SDLK_ESCAPE) {
+            if (event.type == SDL_KEYUP) { 
+                if(event.key.keysym.sym == SDLK_ESCAPE) {
                     running = false;
                 }
-            // if(event.key.keysym.sym == SDLK_ESCAPE)    
+                if(event.key.keysym.sym == SDLK_RIGHT) {
+                    if (indexActive == 5) indexActive = 0;
+                }
+                if(event.key.keysym.sym == SDLK_LEFT) {
+                    if (indexActive == 0) indexActive = 5;
+                }
+                if(event.key.keysym.sym == SDLK_RETURN) {
+                    return indexActive;
+                }
+            }
+            
         }
-        SDL_RenderCopy(renderer, imgBackground, NULL, &backgroundRect);
-        SDL_RenderCopy(renderer, imgAgainBackground, NULL, &againBackground);
-        SDL_RenderCopy(renderer, imgQuitBackground, NULL, &quitBackground);
+            if (indexActive == 5) {
+                SDL_RenderCopy(renderer, border11, NULL, &border1);
+                
+            }
+            if (indexActive == 0) {
+                SDL_RenderCopy(renderer, border22, NULL, &border2);
+            }
+            
+        
+       
         usleep(100);
         SDL_RenderPresent(renderer);
     }
+    
     IMG_Quit();
     return 0;
 }
--- src/showStartMenu.c
+++ src/showStartMenu.c
@@ -28,6 +28,12 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
     SDL_Texture *button33 = IMG_LoadTexture(renderer, "sprites/button.png");
     SDL_Rect button4 = {1160, 935, 400, 120};
     SDL_Texture *button44 = IMG_LoadTexture(renderer, "sprites/button.png");
+    //Music
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    Mix_Chunk *swap = Mix_LoadWAV("music/phaserUp5.wav");
+    Mix_Music *background = Mix_LoadMUS("music/Blind Shift.mp3");
+    Mix_PlayMusic(background, -1);
+    Mix_VolumeMusic(90);
 
     //background 
     SDL_Rect backgroundRect = {0, 0, WIDTH_PIX, HEIGHT_PIX};
@@ -41,24 +47,34 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
         while(SDL_PollEvent(&event)) {
               if (event.type == SDL_KEYUP) {
                 if(event.key.keysym.sym == SDLK_ESCAPE) {
+                    Mix_PlayChannel(-1,swap, 0);
                     running = false;
                 }
                 if(event.key.keysym.sym == SDLK_UP) {
+                    Mix_PlayChannel(-1,swap, 0);
+                    Mix_Volume(-1, 24);
                     if(indexActive == 2) indexActive = 1;
                     if(indexActive == 4) indexActive = 2;
                     if(indexActive == 3) indexActive = 2;
                 }
                 if(event.key.keysym.sym == SDLK_RIGHT) {
+                    Mix_PlayChannel(-1,swap, 0);
+                    Mix_Volume(-1, 24);
                     if(indexActive == 4) indexActive = 3;
                 }
                 if(event.key.keysym.sym == SDLK_DOWN) {
+                    Mix_PlayChannel(-1,swap, 0);
+                    Mix_Volume(-1, 24);
                     if(indexActive == 2) indexActive = 4;
                     if(indexActive == 1) indexActive = 2;
                 }
                 if(event.key.keysym.sym == SDLK_LEFT) {
+                    Mix_PlayChannel(-1,swap, 0);
+                    Mix_Volume(-1, 24);
                     if(indexActive == 3) indexActive = 4;
                 }
                 if(event.key.keysym.sym == SDLK_RETURN) {
+                    Mix_PlayChannel(-1,swap, 0);
                     return indexActive;
                 }
               }
@@ -110,6 +126,9 @@ int showStartMenu(SDL_Renderer *renderer, t_player *player1, t_player *player2) 
         usleep(100);
         SDL_RenderPresent(renderer);
     }
+    Mix_FreeMusic(background);
+    Mix_FreeChunk(swap);
+    Mix_CloseAudio();
     SDL_DestroyTexture(imgBackground);
     SDL_DestroyRenderer(renderer);
     //IMG_Quit();
