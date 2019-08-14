08a258f776e4bfb97577c13ea0af264fba7eba07
Vladislav Balachevskyi <vbalachevs@e2r6p8.unit.ua>
2019-08-14 16:56:30 +0300

    8973

--- src/initPlayers.c
+++ src/initPlayers.c
@@ -1,8 +1,8 @@
 #include "header.h"
 
 void initPlayers(t_player **player1, t_player **player2) {
-    t_player *players1 = (t_player*) malloc(sizeof(t_player) * 100000);
-    t_player *players2 = (t_player*) malloc(sizeof(t_player) * 100000);
+    t_player *players1 = (t_player*) malloc(sizeof(t_player));
+    t_player *players2 = (t_player*) malloc(sizeof(t_player));
     players1->x = WIDTH_SYMB / 4 - 1;
     players1->y =  HEIGHT_SYMB - 2;
     players1->cloneY = 1;
@@ -10,6 +10,8 @@ void initPlayers(t_player **player1, t_player **player2) {
     players1->chClone = 'a';
     players1->pathToTexture = (char*) malloc (100);
     players1->pathToTexture = "sprites/playerShip2_red.png";
+    players1->games = 0;
+    players1->win = 0;
     players1->speed = 1;
     players2->bulletSpeed = players1->bulletSpeed = 0.35;
     players2->coolDown = players1->coolDown  = 180000;
@@ -19,6 +21,8 @@ void initPlayers(t_player **player1, t_player **player2) {
     players2->y = HEIGHT_SYMB - 2; 
     players2->speed = 1;
     players2->cloneY = 1;
+    players2->games = 0;
+    players2->win = 0;
     players2->pathToTexture = (char*) malloc (100);
     players2->pathToTexture = "sprites/playerShip1_blue.png";
     players1->pathToBullet = "sprites/bull_red.png";
